80f46a9e02216379d96c65f6853991c55188167d
dyik <51643039+dyik@users.noreply.github.com>
2019-12-03 13:53:30 +0700

    Update listener.sh

--- listener.sh
+++ listener.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-#ACCOUNT SID : ACa8c0040bb540fa8e2cd14d95b0f82888
-#AUTH TOKEN : 342ce811752235637f36a5eada5b1f40
+#ACCOUNT SID : ACa8c0040bb540fa8e2cd14d95b0f82888;
+#AUTH TOKEN : 342ce811752235637f36a5eada5b1f40;
+#incoming phone numbers : PN531fc3a1b14ae13bb87c9a9947a2d294;
 echo "
 <--www.twilio.com-->
 ####################
@@ -10,13 +11,13 @@ echo "
 
 Copyright  © 2019 TWILIO INC";
 echo "";
-read -p "Server   : https://www.twilio.com/console/phone-numbers/PN531fc3a1b14ae13bb87c9a9947a2d294
+read -p "Server   : https://www.twilio.com/console/phone-numbers/PN531fc3a1b14ae13bb87c9a9947a2d294/";
 read -p "Log File : " messages
 read -p "Delay (2-5) : " delay
 echo "";
 echo "[+] Collecting Information...";
 sleep 3;
-echo "[+] Server : https://www.twilio.com/console/phone-numbers/PN531fc3a1b14ae13bb87c9a9947a2d294/messages";
+echo "[+] Server : https://www.twilio.com/console/phone-numbers/PN531fc3a1b14ae13bb87c9a9947a2d294/";
 sleep 1;
 echo "[+] File   : $messages";
 sleep 1;
@@ -26,7 +27,7 @@ echo "[+] Executing https://www.twilio.com/console/phone-numbers/PN531fc3a1b14ae
 sleep 3;
 while [ true ]
 do
-curl https://www.twilio.com/console/phone-numbers/PN531fc3a1b14ae13bb87c9a9947a2d294/messages
+curl -XPOST https://www.twilio.com/console/phone-numbers/PN531fc3a1b14ae13bb87c9a9947a2d294/messages";
 sleep $delay
-POST https://www.twilio.com/console/phone-numbers/PN531fc3a1b14ae13bb87c9a9947a2d294/messages
+POST https://www.twilio.com/console/phone-numbers/PN531fc3a1b14ae13bb87c9a9947a2d294/messages";
 done
