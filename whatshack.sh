dd0e28d7039a6df879fb0dedaed0de93cd31b4c7
dyik <51643039+dyik@users.noreply.github.com>
2019-12-03 13:42:49 +0700

    Update whatshack.sh

--- whatshack.sh
+++ whatshack.sh
@@ -1,5 +1,6 @@
 #!/bin/hash
-#ACCOUNT SID="ACa8c0040bb540fa8e2cd14d95b0f82888";       #incoming phone numbers = "https://www.twilio.com/console/phone-numbers/instance/PN531fc3a1b14ae13bb87c9a9947a2d294/messages";
+#ACCOUNT SID="ACa8c0040bb540fa8e2cd14d95b0f82888";       
+#incoming phone numbers = "https://www.twilio.com/console/phone-numbers/instance/PN531fc3a1b14ae13bb87c9a9947a2d294/messages";
 #AUTH TOKEN: "342ce811752235637f36a5eada5b1f40";
 echo "
 <--www.twilio.com-->
@@ -22,7 +23,7 @@ echo "Target => $target";
 sleep 0.5
 echo "[+] Exploiting...";
 echo "Nomer => $nomer" >> curl -XPOST https://api.twilio.com/2010-04-01/Accounts/ACa8c0040bb540fa8e2cd14d95b0f82888/IncomingPhoneNumbers/PN531fc3a1b14ae13bb87c9a9947a2d294.json \ --data-urlencode "SmsUrl=http://www.mysite.com/twilio" \ -u 342ce811752235637f36a5eada5b1f40:your_auth_token
-curl -XPOST https://api.twilio.com/2010-04-01/Accounts/ACa8c0040bb540fa8e2cd14d95b0f82888/IncomingPhoneNumbers/PN531fc3a1b14ae13bb87c9a9947a2d294.json \ --data-urlencode "SmsUrl=http://www.mysite.com/twilio" \ -u 342ce811752235637f36a5eada5b1f40:your_auth_token
+POST https://api.twilio.com/2010-04-01/Accounts/ACa8c0040bb540fa8e2cd14d95b0f82888/IncomingPhoneNumbers/PN531fc3a1b14ae13bb87c9a9947a2d294.json \ --data-urlencode "SmsUrl=http://www.mysite.com/twilio" \ -u 342ce811752235637f36a5eada5b1f40:your_auth_token
 sleep 5
 echo "[+] Done.";
 echo "Tunggu 5-10 Menit Untuk Mendapatkan Code.";
@@ -36,8 +37,8 @@ sleep 0.5
 echo "Target => $target";
 sleep 0.5
 echo "[+] Sending Exploit...";
-echo "Code => $code" >> https://api.twilio.com/2010-04-01/Accounts/ACa8c0040bb540fa8e2cd14d95b0f82888/IncomingPhoneNumbers/PN531fc3a1b14ae13bb87c9a9947a2d294.json \ --data-urlencode "SmsUrl=http://www.mysite.com/twilio" \ -u 342ce811752235637f36a5eada5b1f40:your_auth_token
-curl -XPOST https://api.twilio.com/2010-04-01/Accounts/ACa8c0040bb540fa8e2cd14d95b0f82888/IncomingPhoneNumbers/PN531fc3a1b14ae13bb87c9a9947a2d294.json \ --data-urlencode "SmsUrl=http://www.mysite.com/twilio" \ -u 342ce811752235637f36a5eada5b1f40:your_auth_token
+echo "Code => $code" >> https://api.twilio.com/2010-04-01/Accounts/ACa8c0040bb540fa8e2cd14d95b0f82888/IncomingPhoneNumbers/PN531fc3a1b14ae13bb87c9a9947a2d294.json \ --data-urlencode "SmsUrl=http://www.mysite.com/twilio" \ -u 342ce811752235637f36a5eada5b1f40:your_auth_token , 
+POST https://api.twilio.com/2010-04-01/Accounts/ACa8c0040bb540fa8e2cd14d95b0f82888/IncomingPhoneNumbers/PN531fc3a1b14ae13bb87c9a9947a2d294.json \ --data-urlencode "SmsUrl=http://www.mysite.com/twilio" \ -u 342ce811752235637f36a5eada5b1f40
 sleep 5
 echo "[+] Success Exploited!";
 exit;
